239559a377ada50d7e7af4b6c0966a79a4d79034
Enzuz-s <EnhzuzHD@gmail.com>
2022-07-02 15:58:28 +0200

    update

--- YouMain.py
+++ YouMain.py
@@ -23,11 +23,12 @@ def run():
         ydl_opts = {
             'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio/best',
             'writesubtitles': True,  # Adds a subtitles file if it exists
-            'writeautomaticsub': True,  # Adds auto-generated subtitles file
-            'subtitle': '--write-sub --sub-lang en',  # writes subtitles file in english
-            'subtitlesformat': 'srt',  # writes the subtitles file in "srt" or "ass/srt/best"
+            'subtitle': '--embed-subs --sub-lang en',  # writes subtitles file in english
             'download_archive': 'downloaded_songs.txt',
             'outtmpl': data + '/%(title)s.%(ext)s',
+            'postprocessors': [{
+                'key': 'FFmpegSubtitlesConvertor',
+                'format': 'srt',}],
         }
 
         with yb.YoutubeDL(ydl_opts) as ydl:
