58b043c486959f3ddbf6faa7b51e432f11ee1e3c
Enzuz-s <mitchellbakx11@gmail.com>
2023-03-25 19:40:36 +0100

    remade the thunbnail move

--- YouMain.py
+++ YouMain.py
@@ -1,18 +1,16 @@
+import logging
 import os
-import shutil
+import re
+import subprocess
 import sys
 import time
-import traceback
 from os import system, name
-import logging
-import yt_dlp
 
-import subprocess
+import yt_dlp
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 logger = logging.getLogger(__name__)
-# File extensions to move to the "thumbnail" directory
 FILE_EXTENSIONS = ['.webp', '.png', 'jpg']
 
 
@@ -24,8 +22,8 @@ def check_package_updates():
     try:
         with open(os.devnull, "w") as fnull:
             packages = subprocess.check_output(["pip", "list", "--outdated"], stderr=fnull)
-    except subprocess.CalledProcessError as e:
-        raise PackageUpdateError(f"Error checking outdated packages: {e}") from e
+    except subprocess.CalledProcessError as ss:
+        raise PackageUpdateError(f"Error checking outdated packages: {ss}") from ss
 
     packages = packages.decode().split("\n")[2:-1]
 
@@ -41,19 +39,15 @@ def check_package_updates():
                 try:
                     subprocess.check_call(["pip", "install", "--upgrade", package_name], stdout=fnull, stderr=fnull)
                     print(f"Successfully updated {package_name}")
-                except subprocess.CalledProcessError as e:
-                    raise PackageUpdateError(f"Error updating package {package_name}: {e}") from e
+                except subprocess.CalledProcessError as dd:
+                    raise PackageUpdateError(f"Error updating package {package_name}: {dd}") from dd
     else:
         print("All packages are up to date")
 
 
 def run():
-    # Loop indefinitely to download multiple videos
     while True:
-        # Get the URL of the YouTube video from the user
         video_url = input("\nplease enter youtube video url: ")
-        # noinspection SpellCheckingInspection
-        # Set the options for the yt_dlp library
         ydl_opts = {
             'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio/best',
             'download_archive': 'downloaded_songs.txt',
@@ -74,48 +68,43 @@ def run():
             ],
         }
 
-        # Error message to log if an error occurs while downloading the video
         url_error = 'Error. Moving to next URL.'
         try:
-            # Use the yt_dlp library to download the video and its thumbnail
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 error_code = ydl.download([video_url])
 
-                # Check if any videos failed to download, and log an error if so
                 if error_code:
                     print('Some videos failed to download ')
                 else:
                     print("\nDownload complete... ")
-                    # Move the thumbnail to the appropriate directory
-                    thumbnail_path(FILE_EXTENSIONS)
-                    # Clear the screen and ask the user if they want to download another video
+                    move_with_thumbnail(sanitize, os.path.join(sanitize, "thumbnail"), FILE_EXTENSIONS)
                     clear()
         except yt_dlp.utils.DownloadError:
             logger.error(url_error)
             return False
 
 
-def thumbnail_path(file_extensions):
+def move_with_thumbnail(source, dest, file_extensions):
     thumbnail_dir = os.path.join(os.getcwd(), "thumbnails")
     if not os.path.exists(thumbnail_dir):
         os.makedirs(thumbnail_dir)
 
-    for root, dirs, files in os.walk(os.getcwd()):
+    for root, dirs, files in os.walk(source):
         for file in files:
             if any(file.endswith(ext) for ext in file_extensions):
                 source_path = os.path.join(root, file)
-                thumbnail_name = re.sub(r'\.\w+$', '.jpg', file, count=1)
+                thumbnail_name = re.sub(r'\.\w+$', '.' + file_extensions, file, count=1)
                 thumbnail_path = os.path.join(thumbnail_dir, thumbnail_name)
 
-                # Check for path traversal exploits
-                if os.path.realpath(source_path).startswith(os.getcwd()):
+                if os.path.realpath(source_path).startswith(os.path.realpath(source)):
                     with open(source_path, 'rb') as src, open(thumbnail_path, 'wb') as dst:
                         dst.write(src.read())
                     os.remove(source_path)
 
+    os.rename(source, dest)
+
 
 def close():
-    # Wait for one second before closing the program
     time.sleep(0)
     print('\nBye')
     time.sleep(1)
@@ -123,55 +112,42 @@ def close():
 
 
 def clear():
-    # Loop indefinitely until the user provides a valid response
     while True:
-        # Ask the user if they want to download another video
         ans = input("\nDo you want to start again? (y/n) ")
-        # If the user responds with "y", clear the screen and start the download process again
         if ans.lower() == "y":
             system('cls' if name == 'nt' else 'clear')
             time.sleep(0)
             run()
-        # If the user responds with "n", clear the screen and close the program
         elif ans.lower() == 'n':
             system('cls' if name == 'nt' else 'clear')
             close()
-        # If the user provides any other response, ask them to try again
         else:
             print("Please respond with 'Yes' or 'No'\n")
 
 
-# If the script is run directly (i.e. not imported as a module), run the main download process
 if __name__ == '__main__':
-    # noinspection PyBroadException
     try:
         check_package_updates()
 
         data = input("Enter file location to save files: ")
-        # Remove any trailing or leading white spaces
         data = data.strip()
-        # Check the operating system and use the appropriate path separator
         if os.name == 'nt':
             data = data.replace("\\", "/")
         else:
             data = data.replace("\\", os.sep)
         sanitize = os.path.normpath(data)
-        # Check if the path exists and is a directory
         if not os.path.isdir(sanitize):
             print("Error: Invalid file location")
         else:
-            # Print the sanitized input
             print("Thumbnails will be moved to " + os.path.join(sanitize, "thumbnail"))
 
         run()
     except KeyboardInterrupt:
-        # If the user interrupts the process (e.g. with Ctrl+C), log an error and ask if they want to try again
         print('\nInterrupted')
         while True:
             clear()
-    except Exception:
-        # If any other error occurs, log it to a file and print a message to the user
-        with open("log.txt", "w") as log:
-            traceback.print_exc(file=log)
-            print('\nError is printed to log.txt')
-            close()
+
+    except (FileNotFoundError, PermissionError) as e:
+        logger.error(f"Error occurred: {e}")
+        time.sleep(10)
+        close()
