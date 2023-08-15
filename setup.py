ffa6ebe95623e3d9c06f3e1ab53b28710ac32692
Enzuz-s <mitchellbakx11@gmail.com>
2023-08-15 11:48:11 +0200

    Setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='YouTube-',
+    version='1',
+    packages=[''],
+    url='https://github.com/Enzuz-s/YouTube-',
+    license='',
+    author='Enzuz-s',
+    author_email='',
+    description='YouTube Downloader'
+)
