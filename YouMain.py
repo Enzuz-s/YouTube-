8828b79dc99ea894b63634fe90a31f290148dc45
Enzuz-s <mitchellbakx11@gmail.com>
2023-11-25 09:42:08 +0100

    remove package update

--- YouMain.py
+++ YouMain.py
@@ -1,76 +1,70 @@
 import logging
 import os
-import re
+import shutil
 import sys
 import time
 from os import system, name
-
 import yt_dlp
+from yt_dlp.utils import DownloadError
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 logger = logging.getLogger(__name__)
-FILE_EXTENSIONS = ['.webp', '.png', 'jpg']
 
 
-def run():
-    while True:
-        video_url = input("\nplease enter youtube video url: ")
-        ydl_opts = {
-            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio/best',
-            'download_archive': 'downloaded_songs.txt',
-            'windowsfilenames': True,
-            'outtmpl': sanitize + '/%(title)s.%(ext)s',
-            'writesubtitles': True,
-            'subtitleslangs': ['en', '-live_chat'],
-            'writethumbnail': True,
-            'embedthumbnail': True,
-            'ignoreerrors': True,
-            "error_logger": logger,
-            'postprocessors': [
-                {'key': 'FFmpegMetadata',
-                 'add_metadata': True, },
-                {'key': 'FFmpegEmbedSubtitle'},
-                {'key': 'EmbedThumbnail',
-                 'already_have_thumbnail': True,
-                 },
-            ],
-        }
-
-        url_error = 'Error. Moving on to the next URL.'
+def download_video(video_url, file_location):
+    ydl_opts = {
+        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio/best',
+        'download_archive': 'downloaded_songs.txt',
+        'windowsfilenames': True,
+        'outtmpl': os.path.join(file_location, '%(title)s.%(ext)s'),
+        'writesubtitles': True,
+        'subtitleslangs': ['en', '-live_chat'],
+        'writethumbnail': True,
+        'embedthumbnail': True,
+        'ignoreerrors': True,
+        "error_logger": logger,
+        'postprocessors': [
+            {'key': 'FFmpegMetadata', 'add_metadata': True},
+            {'key': 'FFmpegEmbedSubtitle'},
+            {'key': 'EmbedThumbnail', 'already_have_thumbnail': True},
+        ],
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         try:
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                error_code = ydl.download([video_url])
-
-                if error_code:
-                    print('Some videos could not be downloaded')
-                else:
-                    print("\nDownload completed... ")
-                    move_with_thumbnail(sanitize, os.path.join(sanitize, "thumbnail"), FILE_EXTENSIONS)
-                    clear()
-        except yt_dlp.utils.DownloadError:
-            logger.error(url_error)
-            return False
+            ydl.download([video_url])
+        except DownloadError as e:
+            logger.error(f"Error downloading video: {e}")
 
 
-def move_with_thumbnail(source, dest, file_extensions):
-    thumbnail_dir = os.path.join(os.getcwd(), "thumbnails")
-    if not os.path.exists(thumbnail_dir):
-        os.makedirs(thumbnail_dir)
+def move_thumbnails(source_folder, destination_folder):
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
+    files_to_move = [file for file in os.listdir(source_folder) if file.endswith('.webp')]
+    for file in files_to_move:
+        source_path = os.path.join(source_folder, file)
+        destination_path = os.path.join(destination_folder, file)
+        shutil.move(source_path, destination_path)
+        print(f"Moved {file} to {destination_folder}")
+        time.sleep(1)
 
-    for root, dirs, files in os.walk(source):
-        for file in files:
-            if any(file.endswith(ext) for ext in file_extensions):
-                source_path = os.path.join(root, file)
-                thumbnail_name = re.sub(r'\.\w+$', '.' + file_extensions, file, count=1)
-                thumbnail_path = os.path.join(thumbnail_dir, thumbnail_name)
 
-                if os.path.realpath(source_path).startswith(os.path.realpath(source)):
-                    with open(source_path, 'rb') as src, open(thumbnail_path, 'wb') as dst:
-                        dst.write(src.read())
-                    os.remove(source_path)
+def clear_console():
+    system('cls' if name == 'nt' else 'clear')
 
-    os.rename(source, dest)
+
+def start_again(prev_file_location):
+    while True:
+        ans = input("\nDo you want to start again? (y/n) ")
+        if ans.lower() == "y":
+            clear_console()
+            time.sleep(0)
+            run(prev_file_location)
+        elif ans.lower() == 'n':
+            clear_console()
+            close()
+        else:
+            print("Please respond with 'Y' or 'N'\n")
 
 
 def close():
@@ -80,43 +74,46 @@ def close():
     sys.exit()
 
 
-def clear():
+def run(prev_file_location=None):
+    file_location_previous = prev_file_location
     while True:
-        ans = input("\nDo you want to start again? (y/n) ")
-        if ans.lower() == "y":
-            system('cls' if name == 'nt' else 'clear')
-            time.sleep(0)
-            run()
-        elif ans.lower() == 'n':
-            system('cls' if name == 'nt' else 'clear')
-            close()
+        video_url = input("\nPlease enter a YouTube video URL: ")
+
+        if not file_location_previous:
+            file_location = input("\nEnter file location to save files: ").strip()
         else:
-            print("Please respond with 'Yes' or 'No'\n")
+            file_location = input(f"\nEnter file location to save files (default: {file_location_previous}): ").strip()
+            if not file_location:
+                file_location = file_location_previous
+
+        if os.name == 'nt':
+            file_location = file_location.replace("\\", "/")
+        else:
+            file_location = file_location.replace("\\", os.sep)
+        sanitized_location = os.path.normpath(file_location)
+        if not os.path.isdir(sanitized_location):
+            print("Error: Invalid file location")
+            continue
+
+        download_video(video_url, sanitized_location)
+        move_thumbnails(sanitized_location, os.path.join(sanitized_location, 'thumbnails'))
+        clear_console()
+        start_again(sanitized_location)
 
 
 if __name__ == '__main__':
     try:
-        # check_package_updates()
-
-        data = input("Enter file location to save files: ")
-        data = data.strip()
-        if os.name == 'nt':
-            data = data.replace("\\", "/")
-        else:
-            data = data.replace("\\", os.sep)
-        sanitize = os.path.normpath(data)
-        if not os.path.isdir(sanitize):
-            print("Error: Invalid file location")
-        else:
-            print("Thumbnails will be moved to " + os.path.join(sanitize, "thumbnail"))
-
+        clear_console()
         run()
     except KeyboardInterrupt:
         print('\nInterrupted')
         while True:
-            clear()
+            clear_console()
 
     except (FileNotFoundError, PermissionError) as e:
         logger.error(f"Error occurred: {e}")
         time.sleep(10)
         close()
+    except OSError:
+        print("Error: Unrecognized operating system")
+        sys.exit()
